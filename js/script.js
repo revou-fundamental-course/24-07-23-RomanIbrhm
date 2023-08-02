1e16e9751b903fc1d2ab67498206eb70b50c2009
RomanIbrhm <charenirique@gmail.com>
2023-08-02 18:41:34 +0800

    Last init

--- js/script.js
+++ js/script.js
@@ -1,54 +1,32 @@
+// Baru bangen belajar JS pas week 2 day 1 di kelas sefc jadi gapaham per-JS-an
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const destination = document.getElementById('dstination')
+const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classlist.add('error')
-    inputControl.classlist.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error')
-
-    errorDisplay.innerText = ''; 
-    inputControl.classlist.add('success');
-    inputControl.classlist.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const destinatonValue = destination.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (username.value === '' || username.value == null) {
+        messages.push('Username is required')
     }
 
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
+    if (email.value === '' || email.value == null) {
+        messages.push('Email cannot be blank');
+    } else if(!isEmail(emailvalue)) {
+        messages.push('Email cannot be blank');
+    }
+    
+    if (destination.value === '' || destination.value == null) {
+        messages.push('Destination cannot be blank');
     }
 
-};
+    if (messages.length > 0 )
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+})
+
+function isEmail(email) {
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
